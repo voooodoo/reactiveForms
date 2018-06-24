40707ebda1ad351141696666d77facf9f015d8c6
eugene <evgen.zaharevich@gmail.com>
2018-06-24 10:55:23 +0300

    frist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators,  FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+    `],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      'name': new FormControl('', Validators.required),
+      'pass': new FormControl(''),
+      'gender': new FormControl(''),
+      'subscribe': new FormControl(''),
+      'country': new FormControl(''),
+      'email': new FormControl('', [Validators.email, Validators.required]),
+      'phones': new FormArray([
+        new FormControl('+38', Validators.required),
+      ]) 
+    })
+  }
+  addPhone(){
+    (<FormArray>this.form.controls["phones"]).push(new FormControl("+38", Validators.required));
+  }
+
+  removePhone(index:number) {
+    console.log(index);
+    (<FormArray>this.form.controls["phones"]).removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
